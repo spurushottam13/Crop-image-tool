6a999037fc62cf2b7b8fbf9151645ac46696721d
Purushottam Shukla <purushottam.shukla@praktice.ai>
2020-05-16 01:12:25 +0530

    [added] storing viewId in localStorage

--- src/component/ImageContainer/index.js
+++ src/component/ImageContainer/index.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import './imageContainer.css'
 
-function ImageContainer({src, decription}){
+function ImageContainer({src}){
+    const [size, setSize] = useState(null)
+    const imgRef= useRef()
+    useEffect(() => {
+        if(imgRef.current){
+            imgRef.current.onload = () => {
+                setSize({
+                    width: imgRef.current.offsetWidth,
+                    height: imgRef.current.offsetHeight
+                })
+            }
+        }
+        console.log(imgRef.current)
+    })
     return(
         <div className="image-container">
-            <div className="header">
-                {decription}
+            <div className="img-header">
+                {
+                    !size ? (
+                        <span>Loading . . .</span>
+                    ) : (
+                        <div className="inline-wrapper -sp">
+                            <div>Size: {size.width}x{size.height}</div>
+                            <a className="clean-aTag" target="_blank" href={src} >Link</a>
+                        </div>
+                    )
+                }
             </div>
-            <img src={src} className="img" />
+            <img src={src} className="img" ref={imgRef} />
         </div>
     )
 }
